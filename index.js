5c10795658f332b6a2b18466a8d2f0ea6752b723
Enzo Cañete <enzodev4@gmail.com>
2024-12-18 12:09:06 -0300

    Added main functionality and two more functions.
    One to add a product to the shooping list and the second one to shorten titles.

---
+++ index.js
@@ -0,0 +1,120 @@
+let booksContainer = document.getElementById('books-container');
+let currentTopic = document.getElementById('current-topic');
+let cartLinks = document.querySelectorAll('.cart-link');
+document.addEventListener('DOMContentLoaded', () => {
+	fetchBooks();
+});
+
+function agregarAlCarrito(book) {
+	let bookData = book;
+
+	let cartContent = JSON.parse(localStorage.getItem('cart')) || [];
+	let bookSaved = cartContent.find((book) => book.t === bookData.t);
+
+	if (bookSaved === undefined) {
+		cartContent.push(bookData);
+		localStorage.setItem('cart', JSON.stringify(cartContent));
+		updateAmount();
+		alert(`${book.t} ha sido agregado al carrito!`);
+	} else {
+		alert(`${book.t} ya fue agregado al carrito.`);
+	}
+}
+
+function shorterTitles(title) {
+	let newStr = '';
+	const arrayOfString = title.split(' ');
+	for (let i = 0; i < 8; i++) {
+		if (arrayOfString[i] != undefined) {
+			newStr += ` ${arrayOfString[i]}`;
+
+			if (i === 7) {
+				if (
+					newStr.endsWith(',') ||
+					newStr.endsWith(':') ||
+					newStr.endsWith(';')
+				) {
+					newStr = newStr.slice(0, -1) + '';
+				}
+				newStr += '...';
+			}
+		}
+	}
+	return newStr;
+}
+
+function fetchBooks(subject = '', nameToShowcase = 'Mas Populares') {
+	let booksData;
+	booksContainer.innerHTML = `
+							<div class="loader">
+							<div class="earth"></div>
+							<div class="moon"></div>
+						</div>
+`;
+	let thingToSearch;
+	if (subject != '') {
+		thingToSearch = `search=${subject}`;
+	} else {
+		thingToSearch = 'sort';
+	}
+	currentTopic.textContent = 'Cargando...';
+
+	fetch(`http://gutendex.com/books?${thingToSearch}`)
+		// Con "?limit=20" se limita la cantidad de bookos a 20
+		.then((response) => response.json())
+		.then((data) => {
+			booksData = data.results;
+
+			booksContainer.innerHTML = '';
+			for (let i = 0; i < 6; i++) {
+				const randomPrice = Math.floor(Math.random() * (100 - 10 + 1));
+				// agrega cards por cada element del json obtenido en la petición
+				const cardDiv = document.createElement('div');
+
+				const bookData = {
+					i: booksData[i].formats['image/jpeg'],
+					t: shorterTitles(booksData[i].title),
+					a: booksData[i].authors[0].name,
+					p: randomPrice,
+				};
+
+				cardDiv.className = 'book';
+
+				cardDiv.innerHTML = `
+            <div class="card mb-3 rounded-4 pink-borders">
+				<div class="row g-0">
+					<div class="col-md-4">
+						<img
+							src="${bookData.i}"
+							alt="${bookData.t}"
+							class=""
+						/>
+					</div>
+					<div class="col-md-8">
+						<div class="card-body h-100 text-light d-flex flex-column ">
+							<h5 class="book-title card-title mb-0">${bookData.t}</h5>
+							<div class="book-author h6">${bookData.a}</div>
+							<p class="mb-2">Aca iría una sinopsis breve.</p>
+							<div class="card-btn d-flex flex-column mt-auto mb-1">
+								<span class="price mb-2">$${bookData.p}</span>
+								<button class="btn pink-borders focus-ring">Agregar</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+                `;
+
+				//agregar evento al boton "agregar"
+				const btnAdd = cardDiv.querySelector('button');
+				btnAdd.addEventListener('click', () => {
+					//agrega al carrito el book
+					agregarAlCarrito(bookData);
+				});
+
+				booksContainer.appendChild(cardDiv);
+				currentTopic.textContent = nameToShowcase;
+			}
+		})
+		.catch((error) => console.error('Error de conexion', error));
+}
